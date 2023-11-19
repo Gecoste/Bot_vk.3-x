927bc2c13ffea26ed035822f9a8b2026def15b7f
Gecoste <114843030+Gecoste@users.noreply.github.com>
2023-11-19 22:16:22 +1000

    Add files via upload

--- Vkbot.py
+++ Vkbot.py
@@ -7,28 +7,40 @@ vk = vk_api.VkApi(token='vk1.a.F9zyUvx2pmMypVlBBlHk6wD4UGkHnA03Emu3lkrPmf0JiTTqt
 vk.get_api()
 longpoll = VkLongPoll(vk)
 
-def send_message(id, message):
-    vk.method("messages.send", {'chat_id': id, 'message': message, 'random_id': 0})
+def send_message(message, chat_id = None, user_id = None):
+    #Функция для отправки сообщений
+    if chat_id:
+        vk.method("messages.send", {'chat_id': chat_id, 'message': message, 'random_id': 0})
+    if user_id:
+        vk.method("messages.send", {'user_id': user_id, 'message': message, 'random_id': 0})
 
-def user_info(id):
-    return vk.method("users.get", {'user_ids': id})
+def user_info(user_id):
+    return vk.method("users.get", {'user_ids': user_id, 'fields': ['nickname', 'followers_count']})
 
-# Основной цикл
+
 for event in longpoll.listen():
     # Если пришло новое сообщение
     if event.type == VkEventType.MESSAGE_NEW:
         if event.from_chat:
             if event.to_me:
                 if str(event.message).lower() == '/getid':
-                    send_message(event.chat_id, message=f'Ваш личный ID: {event.user_id}')
+                    send_message(chat_id=event.chat_id, message=f'Ваш личный ID: {event.user_id}')
 
                 if str(event.message).lower() == '/stats':
-                    user = user_info(id=event.user_id)
-                    send_message(id=event.chat_id, message=f'''Имя: {user[0]['first_name']}
+                    user = user_info(user_id=event.user_id)
+                    print(user)
+                    send_message(chat_id=event.chat_id, message=f'''@id{event.user_id} ({user[0]['first_name']})
+                    Имя: {user[0]['first_name']}
                     Фамилия: {user[0]['last_name']}
-                    Никнейм: {user[0]['nickname']}
+                    У вас: {user[0]['followers_count']} подписчиков 
                     ''')
 
+    if event.type == VkEventType.CHAT_UPDATE:
+        if event.from_chat:
+            if event.update_type == 6:
+                send_message(chat_id=event.chat_id, message=f'Приветствуем тебя, {event.}')
+            if event.update_type == 7:
+                send_message(chat_id=event.chat_id, message=f'Прощай, {event}')
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
