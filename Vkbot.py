9bdc7f656cf7d3b5cebdd3a71b3ed4e564377b08
Gecoste <114843030+Gecoste@users.noreply.github.com>
2023-11-20 17:24:27 +1000

    Add files via upload

--- Vkbot.py
+++ Vkbot.py
@@ -2,16 +2,19 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardButton, VkKeyboardColor
 
+#[Основные имена] =================================================================
 vk = vk_api.VkApi(token='vk1.a.F9zyUvx2pmMypVlBBlHk6wD4UGkHnA03Emu3lkrPmf0JiTTqtBzvHbn0Ouh-Y-qek7PuAERfpDT-4dYjRhfO97aV4iiwLz8fTxa9bYbw0SsOOSBzZ8K3T4yxODIhGBYiUuuVyJBH3dRy5tvZPy0b5FbHdw9S2FEdV2WrH_B6cQCTShi9pHQoyOsadn7aHQW3kO1tuWxh7tynWii4aBM0MA')
 vk.get_api()
 longpoll = VkLongPoll(vk)
-
 admin_id=397675048
 
+#[/kick] =================================================================
 def kick(chat_id, member_id):
+    #Функция кик участника из беседы
     vk.method("messages.send", {'chat_id': chat_id, 'message': f'Участник @id{member_id} был кикнут', 'random_id': 0})
     vk.method("messages.removeChatUser", {'chat_id': chat_id, 'user_id': member_id})
 
+#[Отправка сообщений в чат] =================================================================
 def send_message(message, chat_id = None, user_id = None):
     #Функция для отправки сообщений
     if chat_id:
@@ -19,12 +22,12 @@ def send_message(message, chat_id = None, user_id = None):
     else:
         vk.method("messages.send", {'user_id': user_id, 'message': message, 'random_id': 0})
 
+#[Информация о пользователе /getid] =========================================================
 def user_info(user_id):
     return vk.method("users.get", {'user_ids': user_id, 'fields': ['nickname', 'followers_count']})
 
-
+#[Обработчик событий] =================================================================
 for event in longpoll.listen():
-# Если пришло новое сообщение
     if event.type == VkEventType.MESSAGE_NEW:
         if event.from_chat:
             if event.to_me:
@@ -38,6 +41,7 @@ for event in longpoll.listen():
                                     Фамилия: {user[0]['last_name']}
                                     У вас: {user[0]['followers_count']} подписчиков 
                                     ''')
+                    
                 if str(event.message).lower() == '/kick':
                     kick(chat_id=event.chat_id, member_id=event.from_user)
 
